86d620e5f3039463a47f0f74d7e1ec57bee4681f
hanalee <lehhha0110@gmail.com>
2022-05-18 20:14:31 +0900

    update

--- lazyloading/lazyloading.js
+++ lazyloading/lazyloading.js
@@ -1,28 +1,46 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  let lazyloadImages = document.querySelectorAll('img.lazy');
-  let lazyloadThrottleTimeout;
+  let lazyloadImage; 
 
-  function lazyload () {
-    if(lazyloadThrottleTimeout) {
-      clearTimeout(lazyloadThrottleTimeout);
-    }
-    lazyloadThrottleTimeout = setTimeout(() => {
-      let scrollTop = window.scrollY;
-      lazyloadImages.forEach((value) => {
-        if(value.offsetTop < (window.innerHeight + scrollTop )) {
-          value.src = value.dataset.src;
-          value.classList.remove('lazy');
+  if ('IntersectionObsever' in window) {
+    lazyloadImage = document.querySelectorAll('.lazy');
+    const imageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          let image = entry.target; 
+          image.classList.remove('lazy');
+          imageObserver.unoberve(image);
         }
       });
+    });
 
-      if(lazyloadImages.length == 0) {
-        document.removeEventListener('scroll', lazyload);
-        window.removeEventListener('resize', lazyload);
-        window.removeEventListener('orientationchange', lazyload);
+    lazyloadImage.forEach(image => imageObserver.observe(image));
+  } else {
+    let lazyloadThrottleTimeout;
+
+    lazyloadImage = document.querySelectorAll('.lazy');
+
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
       }
-    }, 20);
+
+      lazyloadThrottleTimeout = setTimeout(()=>{
+        let scrollTop = window.scrollY; 
+        lazyloadImage.forEach( img =>  {
+          if (img.offsetTop < (window.innerHeight + scrollTop)) {
+            img.src = img.dataset.src;
+            img.classList.remove('lazy'); 
+          }
+        });
+        if (lazyloadImage.length == 0) {
+          document.removeEventListener('scroll', lazyload);
+          window.removeEventListener('resize', lazyload);
+          window.removeEventListener('orientationchange', lazyload);
+        }
+      }, 20);
+    }
+    document.addEventListener('scroll', lazyload);
+    window.addEventListener('resize', lazyload);
+    window.addEventListener('orientationchange', lazyload);
   }
-  document.addEventListener('scroll', lazyload);
-  window.addEventListener('resize', lazyload);
-  window.addEventListener('orientationchange', lazyload);
-});
+})
