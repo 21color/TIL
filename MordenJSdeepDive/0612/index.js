56b706325bda61ebd7f93f4325c25b03f792631e
21color <lehhha0110@gmail.com>
2022-06-12 23:56:20 +0900

    this is Optional chaining

--- MordenJSdeepDive/0612/index.js
+++ MordenJSdeepDive/0612/index.js
@@ -34,3 +34,49 @@ const reducedFilter = ( data, keys, fn ) =>
    필터링된 배열에서 Array.protype.map()을 사용하여 
    Array.protype.reduce()를 사용하여 새 개체를 반환하고 Keys 인수로 제공되지 않은 키를 필터링한다. 
    */
+ const user = {
+	name : 'kim',
+		 age : {value: 20}
+ };
+
+console.log(user.age?.value);
+
+let realuser;
+
+console.log( realuser ?? '로딩중' )
+
+const obj = {
+		data : {
+				name : {
+						id : 123,
+						date : 456,
+				},
+
+		},
+}
+
+console.log(obj.data?.name?.age?? '뭐요');
+
+{
+const person1 = {
+		name : 'Hana',
+		job : {
+				title : 'S/W Enginner',
+				manager : {
+						name : 'Bob',
+						},
+				},
+		};
+		const person2 = {
+		name : 'jinYoung',
+		};
+		{
+		function printManager(person) {
+		console.log(person?.job?.manager?.name ?? 'test');
+		}
+		printManager(person1);
+		printManager(person2);
+		}
+}
+
+
